fea7001d1418f24629b317d778c28ecd8823c1db
Mond Wan <mondwan.1015@gmail.com>
2015-02-15 14:20:03 +0800

    Remove hard codes picture and text
    --Overview
    1. There are no more default values inside items in the view XML.
    2. GCMMessageAdapter will insert picture for us from now on.

--- app/src/main/java/com/iems5722/assignment3/GCMMessageAdapter.java
+++ app/src/main/java/com/iems5722/assignment3/GCMMessageAdapter.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.List;
@@ -59,6 +60,11 @@ public class GCMMessageAdapter extends ArrayAdapter<GCMMessage> {
                 R.id.GCMMessageDescription
         );
 
+        ImageView pictureView = (ImageView) convertView.findViewById(
+                R.id.GCMMessagePicture
+        );
+
+        pictureView.setImageResource(R.drawable.model1);
         titleView.setText(msg.getTitle());
         descriptionView.setText(msg.getDescription());
 
